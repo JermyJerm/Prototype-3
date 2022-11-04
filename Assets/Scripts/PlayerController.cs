8687865e66e3fc7ba4b526e50919af3522df8bda
JermyJerm <99265510+JermyJerm@users.noreply.github.com>
2022-11-04 12:46:36 -0400

    half done with part 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    public bool isOnGround = true;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+           playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+           isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
